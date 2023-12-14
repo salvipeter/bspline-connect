f6e65be0d070c8d61d1dd9c75004b947541c4d86
Peter Salvi <vukung@yahoo.com>
2023-12-14 18:24:08 +0100

    Normal curvature bugfix

--- bsconnect.cc
+++ bsconnect.cc
@@ -13,7 +13,9 @@ static Vector3D surfaceNormal(const BSSurface &s, double u, double v) {
 // `p` (supposedly in the `(u,v)` plane) is written as a combination of `u` and `v`.
 // Returns `{ a, b }` s.t. `u * a + v * b = p`.
 static std::pair<double, double> inSystem(const Vector3D &u, const Vector3D &v, const Vector3D &p) {
-  return { p * u / u.normSqr(), p * v / v.normSqr() };
+  double u2 = u * u, v2 = v * v, pu = p * u, pv = p * v, uv = u * v;
+  double denom = u2 * v2 - uv * uv;
+  return { (v2 * pu - uv * pv) / denom, (u2 * pv - uv * pu) / denom };
 }
 
 static double normalCurvature(const BSSurface &s, double u, double v, const Vector3D &dir) {
